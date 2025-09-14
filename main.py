b110b620e6c152b1b62b88dda25625fd7dde0a5b
samoateng28 <s_amoateng@mail.fhsu.edu>
2025-09-13 23:04:06 -0500

    i modified the code to add the chart directory if it does not exist

--- main.py
+++ main.py
@@ -7,8 +7,8 @@
 # (20/20 points) Using an API of your choice (yfinance works), collect the closing price of 5 of your favorite stock tickers for the last 10 trading days. -- Done
 # (10/10 points) Store this information in a list that you will convert to a array in NumPy. - Done
 # (10/10 points) Plot these 5 graphs. Feel free to add as much information to the graphs as you like exploring the documentation for matplotlib. At minimum it just needs to show 10 data points. -- Done
-# (10/10 points) Save these graphs in a folder called charts as PNG files. Do not upload these to your project folder, the project should save these when it executes. You may want to add this folder to your .gitignore file.
-# (10/10 points) There should be a minimum of 5 commits on your project, be sure to commit often!
+# (10/10 points) Save these graphs in a folder called charts as PNG files. Do not upload these to your project folder, the project should save these when it executes. You may want to add this folder to your .gitignore file. --Done
+# (10/10 points) There should be a minimum of 5 commits on your project, be sure to commit often! --Done
 # (10/10 points) I will be checking out the main branch of your project. Please be sure to include a requirements.txt file which contains all the packages that need installed. You can create this fille with the output of pip freeze at the terminal prompt.
 # (20/20 points) There should be a README.md file in your project that explains what your project is, how to install the pip requirements, and how to execute the program. Please use the GitHub flavor of Markdown.
 
@@ -18,6 +18,12 @@ import pprint
 import numpy as np
 import matplotlib.pyplot as plt
 import  copy
+from pathlib import Path
+
+charts = Path('charts')
+if not charts.exists():
+    Path(r'charts').mkdir()
+
 
 mystocks = ['AAPL', 'MSFT', 'NVDA', 'GOOG', 'TSLA']
 mystockdata = {}
